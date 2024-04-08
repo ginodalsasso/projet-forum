743f7b5762470d01878530a65d9a8855fcc168ff
kame-sennin23 <dalsasso.gino@gmail.com>
2024-04-08 14:04:45 +0200

    model\entities\User getters and setters

--- model/entities/User.php
+++ model/entities/User.php
@@ -11,6 +11,9 @@ final class User extends Entity{
 
     private $id;
     private $nickName;
+    private $password;
+    private $email;
+    private $role;
 
     public function __construct($data){         
         $this->hydrate($data);        
@@ -47,6 +50,66 @@ final class User extends Entity{
      */ 
     public function setNickName($nickName){
         $this->nickName = $nickName;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of password
+     */ 
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Set the value of password
+     *
+     * @return  self
+     */ 
+    public function setPassword($password)
+    {
+        $this->password = $password;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of email
+     */ 
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    /**
+     * Set the value of email
+     *
+     * @return  self
+     */ 
+    public function setEmail($email)
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of role
+     */ 
+    public function getRole()
+    {
+        return $this->role;
+    }
+
+    /**
+     * Set the value of role
+     *
+     * @return  self
+     */ 
+    public function setRole($role)
+    {
+        $this->role = $role;
 
         return $this;
     }
