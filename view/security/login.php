2cf41ffafe2eeec3806d85227c0557c1d7465ea1
kame-sennin23 <dalsasso.gino@gmail.com>
2024-04-17 19:54:44 +0200

    finishing css forms

--- view/security/login.php
+++ view/security/login.php
@@ -1,11 +1,13 @@
 <h1>Se connecter</h1>
+<div class="form_display">
     <form action="index.php?ctrl=security&action=addLogin" method="POST" enctype="multipart/form-data">
-        
+
         <label for="pseudo">Pseudo</label>
         <input type="pseudo" name="pseudo" id="pseudo" required><br>
-        
+
         <label for="password">Mot de passe</label>
         <input type="password" name="password" id="password" value="Goku67540//!" required><br>
-        
+
         <input type="submit" name="submit" value="Se connecter">
     </form>
+</div>
